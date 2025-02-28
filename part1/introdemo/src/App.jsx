c92bf5a5193567d47e1a8b178468c61a543d05fc
Sunrise443 <sunkensun0@gmail.com>
2025-02-28 19:49:34 +0300

    part1c

--- part1/introdemo/src/App.jsx
+++ part1/introdemo/src/App.jsx
@@ -2,18 +2,37 @@ import { useState } from 'react'
 
 const App = () => {
   const [ counter, setCounter ] = useState(0)
-
-  setTimeout(
-    () => setCounter(counter + 1),
-    1000
-  )
-
-
   console.log('rendering...', counter)
 
+  const increaseByOne = () => {
+    setCounter(counter + 1)
+    console.log('rendered before', counter)
+  }
+  const decreaseByOne = () => {
+    setCounter(counter - 1)
+    console.log('renderd before', counter)
+  }
+  const setToZero = () => {
+    setCounter(0)
+    console.log('rendered before', counter)
+  }
+
   return (
-    <div>{counter}</div>
+    <>
+      <Display counter={counter}/>
+      <Button text='plus' onClick={increaseByOne}/>
+      <Button text='zero' onClick={setToZero}/>
+      <Button text='minus' onClick={decreaseByOne}/>
+    </>
   )
 }
+
+const Display = ({ counter }) => <div>{counter}</div>
+
+const Button = ({ text, onClick }) => (
+    <button onClick={onClick}>
+      {text}
+    </button>
+)
 
 export default App
